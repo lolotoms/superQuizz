1505657705a63038a60c968123cb36a38ec31dd9
Laure Thomas <laure.thomas@dailymotion.com>
2024-09-05 10:32:04 +0200

    third chapter

--- app/src/main/java/com/example/superquizz/WelcomeFragment.java
+++ app/src/main/java/com/example/superquizz/WelcomeFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -73,7 +77,39 @@ public class WelcomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.playButton.setEnabled(false);
+        binding.welcomeFragmentPlayButton.setEnabled(false);
+        binding.welcomeFragmentUsernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.welcomeFragmentPlayButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+        binding.welcomeFragmentPlayButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                QuizzFragment quizFragment = QuizzFragment.newInstance();
+                fragmentTransaction.add(R.id.fragment_container_view, quizFragment);
+                //fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.welcomeFragmentPlayButton.setEnabled(false);
     }
 }
