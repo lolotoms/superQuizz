a290fbf6d38cacd37b1d8f1c3440df706784b637
Laure Thomas <laure.thomas@dailymotion.com>
2024-09-05 09:14:20 +0200

    third chapetr

--- app/src/main/java/com/example/superquizz/WelcomeFragment.java
+++ app/src/main/java/com/example/superquizz/WelcomeFragment.java
@@ -2,11 +2,16 @@ package com.example.superquizz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.superquizz.databinding.ActivityMainBinding;
+import com.example.superquizz.databinding.FragmentWelcomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,8 @@ public class WelcomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FragmentWelcomeBinding binding;
 
     public WelcomeFragment() {
         // Required empty public constructor
@@ -53,12 +60,20 @@ public class WelcomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
+
     }
 }
